06c11eca90ee56c8f749ea65d3d6833b89527840
Unal Ege Gaznepoglu <uenal.ege.gaznepoglu@iis.fraunhofer.de>
2023-12-19 11:37:05 +0100

    Fixes to the anonymizer classes - BaseAnonymizer and its descendants are now dumpable - Improved documentation - The configs now use !PLACEHOLDER tag from HyperPyYAML instead of the custom class

--- anonymization/modules/speaker_embeddings/anonymization/passthrough.py
+++ anonymization/modules/speaker_embeddings/anonymization/passthrough.py
@@ -1,26 +1,36 @@
 from .base_anon import BaseAnonymizer
 import torch
+from typing import Union
+import ruamel.yaml as yaml
+
 
 class Passthrough(BaseAnonymizer):
+    """
+    A 'Passthrough' 'anonymizer' that does not anonymize the speaker embeddings.
+    """
 
-    def __init__(self, vec_type='xvector', device=None, **kwargs):
-        # Base class for speaker embedding anonymization.
-        self.vec_type = vec_type
-        self.suffix = '_res'
+    def __init__(
+        self,
+        vec_type: str = "",
+        device: Union[str, torch.device, int] = "cuda:0",
+        suffix: str = "_res",
+        **kwargs
+    ):
+        super().__init__(vec_type, device, suffix, **kwargs)
 
-        if isinstance(device, torch.device):
-            self.device = device
-        elif isinstance(device, str):
-            self.device = torch.device(device)
-        elif isinstance(device, int):
-            self.device = torch.device(f'cuda:{device}')
-        else:
-            self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-    def anonymize_embeddings(self, speaker_embeddings, emb_level='spk'):
-        # no need to refer to emb_level, 
+    def anonymize_embeddings(
+        self, speaker_embeddings: torch.Tensor, emb_level: str = "spk"
+    ) -> torch.Tensor:
+        """
+        Returns the speaker embeddings unchanged.
+        """
+        # no need to refer to emb_level,
         # as extractor also yields spk-level or utt-level.
         return speaker_embeddings
-    
+
     def to(self, device):
-        self.device = device
+        """
+        Move the anonymizer to the given device. For the passthrough anonymizer,
+        this is a no-op, apart from setting the property.
+        """
+        super().to(device)
--- anonymization/modules/speaker_embeddings/anonymization/pool_anon.py
+++ anonymization/modules/speaker_embeddings/anonymization/pool_anon.py
@@ -3,6 +3,9 @@ import numpy as np
 import torch
 import json
 from tqdm import tqdm
+from typing import Union
+
+from os import PathLike
 from sklearn.metrics.pairwise import cosine_distances
 from sklearn.preprocessing import minmax_scale, StandardScaler
 
@@ -12,42 +15,127 @@ from ..speaker_extraction import SpeakerExtraction
 from ..speaker_embeddings import SpeakerEmbeddings
 from utils import transform_path
 
-REVERSED_GENDERS = {'m': 'f', 'f': 'm'}
+REVERSED_GENDERS = {
+    "m": "f", 
+    "f": "m"
+}
 
 
 class PoolAnonymizer(BaseAnonymizer):
+    """
+    An implementation of the 'Pool' anonymization method, that is based on the
+    primary baseline of the Voice Privacy Challenge 2020.
 
-    def __init__(self, vec_type='xvector', device=None, model_name=None, pool_data_dir='data/libritts_train_other_500',
-                 pool_vec_path='original_speaker_embeddings/pool_embeddings', N=200, N_star=100, distance='plda',
-                 cross_gender=False, proximity='farthest', scaling=None, stats_per_dim_path=None,
-                 distance_model_path='distances/plda/libritts_train_other_500_xvector',
-                 embed_model_dir=None, save_intermediate=False, **kwargs):
-        # Pool anonymization method based on the primary baseline of the Voice Privacy Challenge 2020.
-        # Given a speaker vector, the N most distant vectors in an external speaker pool are extracted,
-        # and an average of a random subset of N_star vectors is computed and taken as new speaker vector.
-        # Default distance measure is PLDA.
-        super().__init__(vec_type=vec_type, device=device)
+    For every source x-vector, an anonymized x-vector is computed by finding 
+    the N farthest x-vectors in an external pool (LibriTTS train-other-500) 
+    according to the PLDA distance, and by averaging N∗ randomly selected
+    vectors among them. In the baseline, we use:
+        N = 200,
+        N∗ = 100.
+    """
+    def __init__(
+        self,
+        vec_type: str = "xvector",
+        device: Union[str, torch.device, int, None] = None,
+        model_name: str = None,
+        pool_data_dir: Union[str, PathLike] = "data/libritts_train_other_500",
+        pool_vec_path: Union[str, PathLike] = "original_speaker_embeddings/pool_embeddings",
+        N: int = 200,
+        N_star: int = 100,
+        distance: str = "plda",
+        cross_gender: bool = False,
+        proximity: bool = "farthest",
+        scaling: str = None,
+        stats_per_dim_path: Union[str, PathLike] = None,
+        distance_model_path: Union[str, PathLike] = "distances/plda/libritts_train_other_500_xvector",
+        embed_model_path: Union[str, PathLike] = None,
+        save_intermediate: bool = False,
+        suffix: str = "_anon",
+        **kwargs,
+    ):
+        """
+        Args:
+            vec_type (str): Type of the speaker embeddings, currently supported
+                are 'xvector', 'ecapa', 'style-embed'.
 
-        self.model_name = model_name if model_name else f'pool_{vec_type}'
+            device (Union[str, torch.device, int, None]): Device to use for
+                the procedure, e.g. 'cpu', 'cuda', 'cuda:0', etc.
+            
+            model_name (str): Name of the model, used for distances that 
+                require a model (e.g., PLDA).
 
-        self.N = N  # number of most distant vectors to consider
-        self.N_star = N_star  # number of vectors to include in averaged vector
-        self.proximity = proximity  # proximity method, either 'farthest' (distant vectors), 'nearest', or 'closest'
-        self.cross_gender = cross_gender  # Whether to reverse the genders of the speakers
+            pool_data_dir (Union[str, PathLike]): Path to the audio data 
+            which will be used for x-vector pool extraction.
+
+            pool_vec_path (Union[str, PathLike]): Path to the stored
+                speaker embeddings of the pool.
+
+            N (int): Number of most 'fitting' vectors to consider.
+
+            N_star (int): Number of vectors to randomly select from the N most
+                'fitting' vectors, to compute the average.
+
+            distance (str): Distance measure, either 'plda' or 'cosine'.
+
+            cross_gender (bool): Whether to switch genders of the speakers
+                during anonymization. 
+
+            proximity (str): Proximity measure, determining which vectors in 
+                the pool are the 'fittest', can be either 'farthest', 
+                'nearest' or 'center'.
+
+            scaling (str): Scaling method to use, can be either 'minmax' or
+                'std'.
+
+            stats_per_dim_path (Union[str, PathLike]): Path to the file
+                containing the statistics for each dimension in the given
+                embedding type.
+
+            distance_model_path (Union[str, PathLike]): Path to the stored
+                distance model (required for PLDA).
+
+            embed_model_path (Union[str, PathLike]): Path to the directory
+                containing the speaker embedding model.
+
+            save_intermediate (bool): Whether to save intermediate results.
+
+            suffix (str): Suffix to append to the output folder names.
+
+        """
+        print(locals())
+        super().__init__(vec_type=vec_type, device=device, suffix=suffix)
+
+        self.model_name = model_name if model_name else f"pool_{vec_type}"
+
+        self.N = N 
+        self.N_star = N_star
+        self.proximity = proximity
+        self.cross_gender = cross_gender
         self.save_intermediate = save_intermediate
 
         # external speaker pool
-        self.pool_embeddings = self._load_pool_embeddings(pool_data_dir=Path(pool_data_dir),
-                                                          pool_vec_path=Path(pool_vec_path),
-                                                          embed_model_dir=Path(embed_model_dir))
-        self.pool_genders = {gender: [i for i, spk_gender in enumerate(self.pool_embeddings.genders)
-                                      if spk_gender == gender] for gender in set(self.pool_embeddings.genders)}
+        self.pool_embeddings = self._load_pool_embeddings(
+            pool_data_dir=Path(pool_data_dir).expanduser(),
+            pool_vec_path=Path(pool_vec_path).expanduser(),
+            embed_model_path=Path(embed_model_path).expanduser(),
+        )
+        self.pool_genders = {
+            gender: [
+                i
+                for i, spk_gender in enumerate(self.pool_embeddings.genders)
+                if spk_gender == gender
+            ]
+            for gender in set(self.pool_embeddings.genders)
+        }
 
         # distance model; PLDA model if distance == plda; None if distance == cosine
         self.distance = distance  # distance measure, either 'plda' or 'cosine'
-        if self.distance == 'plda':
-            self.distance_model = PLDAModel(train_embeddings=self.pool_embeddings,
-                                            results_path=Path(distance_model_path), save_plda=self.save_intermediate)
+        if self.distance == "plda":
+            self.distance_model = PLDAModel(
+                train_embeddings=self.pool_embeddings,
+                results_path=Path(distance_model_path),
+                save_plda=self.save_intermediate,
+            )
         else:
             self.distance_model = None
 
@@ -55,23 +143,32 @@ class PoolAnonymizer(BaseAnonymizer):
         self.scaling = scaling
         self.stats_per_dim_path = stats_per_dim_path or Path()
 
-    def _load_pool_embeddings(self, pool_data_dir, pool_vec_path, embed_model_dir):
+    def _load_pool_embeddings(self, pool_data_dir, pool_vec_path, embed_model_path):
         print(pool_data_dir)
         if pool_vec_path.exists():
-            pool_embeddings = SpeakerEmbeddings(vec_type=self.vec_type, emb_level='spk', device=self.device)
+            pool_embeddings = SpeakerEmbeddings(
+                vec_type=self.vec_type, emb_level="spk", device=self.device
+            )
             pool_embeddings.load_vectors(pool_vec_path)
         else:
-            extraction_settings = {'vec_type': self.vec_type, 'emb_level': 'spk'}
-            emb_extractor = SpeakerExtraction(results_dir=pool_vec_path, model_dir=embed_model_dir, device=self.device,
-                                              settings=extraction_settings, save_intermediate=self.save_intermediate)
-            pool_embeddings = emb_extractor.extract_speakers(dataset_path=pool_data_dir, dataset_name='')
+            extraction_settings = {"vec_type": self.vec_type, "emb_level": "spk", "embed_model_path": embed_model_path}
+            emb_extractor = SpeakerExtraction(
+                results_dir=pool_vec_path,
+                devices=[self.device],
+                settings=extraction_settings,
+                save_intermediate=self.save_intermediate,
+            )
+            pool_embeddings = emb_extractor.extract_speakers(
+                dataset_path=pool_data_dir, dataset_name=""
+            )
         return pool_embeddings
 
-    def anonymize_embeddings(self, speaker_embeddings, emb_level='spk'):
-        distance_matrix = self._compute_distances(vectors_a=self.pool_embeddings.vectors,
-                                                  vectors_b=speaker_embeddings.vectors)
+    def anonymize_embeddings(self, speaker_embeddings: torch.Tensor, emb_level: str = "spk"):
+        distance_matrix = self._compute_distances(
+            vectors_a=self.pool_embeddings.vectors, vectors_b=speaker_embeddings.vectors
+        )
 
-        print(f'Anonymize embeddings of {len(speaker_embeddings)} speakers...')
+        print(f"Anonymize embeddings of {len(speaker_embeddings)} speakers...")
         identifiers = []
         speakers = []
         anon_vectors = []
@@ -83,23 +180,37 @@ class PoolAnonymizer(BaseAnonymizer):
             gender = speaker_embeddings.genders[i]
             distances_to_speaker = distance_matrix[:, i]
             candidates = self._get_pool_candidates(distances_to_speaker, gender)
-            selected_anon_pool = np.random.choice(candidates, self.N_star, replace=False)
-            anon_vec = torch.mean(self.pool_embeddings.speaker_vectors[selected_anon_pool], dim=0)
+            selected_anon_pool = np.random.choice(
+                candidates, self.N_star, replace=False
+            )
+            anon_vec = torch.mean(
+                self.pool_embeddings.speaker_vectors[selected_anon_pool], dim=0
+            )
             identifiers.append(identifier)
             speakers.append(speaker)
             anon_vectors.append(anon_vec)
-            genders.append(gender if not self.cross_gender else REVERSED_GENDERS[gender])
+            genders.append(
+                gender if not self.cross_gender else REVERSED_GENDERS[gender]
+            )
 
-        anon_embeddings = SpeakerEmbeddings(vec_type=self.vec_type, device=self.device, emb_level=emb_level)
-        anon_embeddings.set_vectors(identifiers=identifiers, vectors=torch.stack(anon_vectors, dim=0),
-                                    speakers=speakers, genders=genders)
+        anon_embeddings = SpeakerEmbeddings(
+            vec_type=self.vec_type, device=self.device, emb_level=emb_level
+        )
+        anon_embeddings.set_vectors(
+            identifiers=identifiers,
+            vectors=torch.stack(anon_vectors, dim=0),
+            speakers=speakers,
+            genders=genders,
+        )
 
         return anon_embeddings
 
     def _compute_distances(self, vectors_a, vectors_b):
-        if self.distance == 'plda':
-            return 1 - self.distance_model.compute_distance(enrollment_vectors=vectors_a, trial_vectors=vectors_b)
-        elif self.distance == 'cosine':
+        if self.distance == "plda":
+            return 1 - self.distance_model.compute_distance(
+                enrollment_vectors=vectors_a, trial_vectors=vectors_b
+            )
+        elif self.distance == "cosine":
             return cosine_distances(X=vectors_a.cpu(), Y=vectors_b.cpu())
         else:
             return []
@@ -110,45 +221,49 @@ class PoolAnonymizer(BaseAnonymizer):
         else:
             distances = distances[self.pool_genders[gender]]
 
-        if self.proximity == 'farthest':
-            return np.argpartition(distances, -self.N)[-self.N:]
-        elif self.proximity == 'nearest':
-            return np.argpartition(distances, self.N)[:self.N]
-        elif self.proximity == 'center':
+        if self.proximity == "farthest":
+            return np.argpartition(distances, -self.N)[-self.N :]
+        elif self.proximity == "nearest":
+            return np.argpartition(distances, self.N)[: self.N]
+        elif self.proximity == "center":
             sorted_distances = np.sort(distances)
-            return sorted_distances[len(sorted_distances)//2:(len(sorted_distances)//2)+self.N]
+            return sorted_distances[
+                len(sorted_distances) // 2 : (len(sorted_distances) // 2) + self.N
+            ]
 
     def _load_scaling_ranges(self, stats_per_dim_path):
         if stats_per_dim_path and Path(stats_per_dim_path).exists():
             with open(stats_per_dim_path) as f:
                 dim_ranges = json.load(f)
-                return [(v['min'], v['max']) for k, v in sorted(dim_ranges.items(), key=lambda x: int(x[0]))]
+                return [
+                    (v["min"], v["max"])
+                    for k, v in sorted(dim_ranges.items(), key=lambda x: int(x[0]))
+                ]
         else:
-            raise FileNotFoundError(f'You need to specify a path to an existing file containing the statistics for '
-                                    f'each dimension in the given embedding type, '
-                                    f'stats_per_dim_path={stats_per_dim_path} is not valid!')
+            raise FileNotFoundError(
+                f"You need to specify a path to an existing file containing the statistics for "
+                f"each dimension in the given embedding type, "
+                f"stats_per_dim_path={stats_per_dim_path} is not valid!"
+            )
 
     def _scale_embeddings(self, embeddings):
         vectors = embeddings.vectors.cpu().numpy()
 
-        if self.scaling == 'minmax':
+        if self.scaling == "minmax":
             scaling_ranges = self._load_scaling_ranges(self.stats_per_dim_path)
             scaled_dims = []
             for i in range(len(scaling_ranges)):
-                scaled_dims.append(minmax_scale(vectors[:, i], scaling_ranges[i], axis=0))
+                scaled_dims.append(
+                    minmax_scale(vectors[:, i], scaling_ranges[i], axis=0)
+                )
 
             scaled_vectors = torch.tensor(np.array(scaled_dims)).T.to(self.device)
             embeddings.vectors = scaled_vectors
 
-        elif self.scaling == 'std':
+        elif self.scaling == "std":
             std_scaler = StandardScaler()
             std_scaler.fit(self.pool_embeddings.vectors.cpu().numpy())
             scaled_vectors = torch.tensor(std_scaler.transform(vectors))
             embeddings.vectors = scaled_vectors
 
         return embeddings
-
-# for every source x-vector, an anonymized x-vector is computed by finding the N farthest x-
-# vectors in an external pool (LibriTTS train-other-500) accord-
-# ing to the PLDA distance, and by averaging N ∗ randomly se-
-# lected vectors among them. In the baseline, we use N = 200 and N ∗ = 100
