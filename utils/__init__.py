8edeac7128df8977aa5f4e09412d9c79faaba47c
xiaoxiaomiao323 <152830384+xiaoxiaomiao323@users.noreply.github.com>
2024-01-07 20:43:49 +0800

    Allow for the input of anonymized data directly

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .data_io import read_kaldi_format, save_kaldi_format, parse_yaml, save_yaml, write_table
 from .path_management import create_clean_dir, remove_contents_in_dir, transform_path, get_datasets, \
     find_asv_model_checkpoint, scan_checkpoint
-from .prepare_results_in_kaldi_format import prepare_evaluation_data, combine_asr_data, split_vctk_into_common_and_diverse
+from .prepare_results_in_kaldi_format import prepare_evaluation_data, combine_asr_data, split_vctk_into_common_and_diverse,get_anon_wav_scps
