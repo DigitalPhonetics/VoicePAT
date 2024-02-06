7d00d06db3753878a56f915fa76b4747041a6714
xiaoxiaomiao323 <152830384+xiaoxiaomiao323@users.noreply.github.com>
2024-02-06 20:10:07 +0800

    Update 02_run.sh

--- 02_run.sh
+++ 02_run.sh
@@ -4,15 +4,15 @@ source env.sh
 
 
 
-#generate b2 anonymized audio (libri dev+test set & libri-360h)
+#generate b2 anonymized audio (libri dev+test set - 25min & libri-360h)
 python run_anonymization_dsp.py --config anon_dsp.yaml
 
 #perform libri dev+test pre evaluation using pretrained ASV/ASR models
 #ASV-20mins, ASR-9hours if eval_batchsize=3, ASR-12hours if evel_batchsize=2 (default)
-python run_evaluation.py --config configs/eval_pre_from_anon_datadir.yaml
+python run_evaluation.py --config eval_pre_from_anon_datadir.yaml
 
 
 #train post ASV using anonymized libri-360 and perform libri dev+test post evaluation
 #ASV training takes 2hours
-python run_evaluation.py --config configs/eval_post_scratch_from_anon_datadir.yaml 
+python run_evaluation.py --config eval_post_scratch_from_anon_datadir.yaml 
 
